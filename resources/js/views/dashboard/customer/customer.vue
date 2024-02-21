a6f8599180734e161de21e4480e83bdb2a3f59b5
nazirahmad1 <nazirahmadrahmaty1@gmail.com>
2024-02-21 14:14:38 +0430

    add min-height

--- resources/js/views/dashboard/customer/customer.vue
+++ resources/js/views/dashboard/customer/customer.vue
@@ -4,7 +4,7 @@
 
     <div class="row">
         <div class="col-12">
-            <div class="card h-100">
+            <div class="card h-100" style="min-height:100vh">
                 <div class="card-body">
                     <div class="row mb-2">
                    
